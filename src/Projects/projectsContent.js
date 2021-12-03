1972341683bb529008a18186e35ca91fa8ef4517
Handika Harianto Ew Jong <handikaharianto01@gmail.com>
2021-12-03 20:56:20 +0700

    Add covid-19 tracker to project section

--- src/Projects/projectsContent.js
+++ src/Projects/projectsContent.js
@@ -8,6 +8,15 @@ const projectsContent = [
     githubLink: "https://github.com/handikaharianto/OMDb-api",
     webLink: "https://movie-search-omdb-api.netlify.app/",
   },
+  {
+    id: 2,
+    title: "covid-19 tracker",
+    image: "./images/projects/preview-covid-19-tracker.png",
+    desc: "Covid-19 Tracker with a dashboard-like layout that provides information about Covid-19 worldwide.",
+    techList: ["HTML", "Sass", "JavaScript", "React"],
+    githubLink: "https://github.com/handikaharianto/covid19-tracker",
+    webLink: "https://han-covid-19-tracker.netlify.app/",
+  },
 ];
 
 export default projectsContent;
